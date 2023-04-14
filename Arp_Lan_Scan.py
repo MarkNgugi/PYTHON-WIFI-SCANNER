4ce446273d7d28e631fd0e194063ce599342b890
MarkProxy <111571887+MarkNgugi@users.noreply.github.com>
2023-04-14 16:30:53 -0400

    Update Arp_Lan_Scan.py

--- Arp_Lan_Scan.py
+++ Arp_Lan_Scan.py
@@ -23,7 +23,7 @@ ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
 
 # Get the address range to ARP
 while True:
-    ip_add_range_entered = input("\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ")
+    ip_add_range_entered = input("\nPlease enter the ip address and range that you want to send the ARP request to (eg: 192.168.1.0/24): ")
     if ip_add_range_pattern.search(ip_add_range_entered):
         print(f"{ip_add_range_entered} is a valid ip address range")
         break
